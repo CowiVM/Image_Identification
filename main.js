d8e153baf7ceac072bf0c06c8a06adc5fdaadec8
CowiVM <72988033+CowiVM@users.noreply.github.com>
2021-02-12 20:55:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 Webcam.set({
-    
+    width: 310,
+    height: 300,
+    img_format: "png",
+    png_quality: 90,
+
+    constraints: {
+        facingMode:"environment"
+    }
 })
+
+camera = document.getElementById("camera")
+
+Webcam.attach(camera)
+
+function take_snapshot()
